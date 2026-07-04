065e16120bfc84197736dac4afeb1f21ee26c48c
Kyndansky <davidericcobene10@gmail.com>
2026-07-04 17:43:13 +0200

    Deleted an old test scene, worked on customer npc manager and costumer npc general logic.

--- 3d-food-tycoon/managers/customer_npcs_manager.gd
+++ 3d-food-tycoon/managers/customer_npcs_manager.gd
@@ -2,18 +2,24 @@
 extends Node
 class_name CustomerNpcManager
 
+@export var max_no_order_customers:int=5
 var is_initialized: bool = false
 var waiting_spots_positions: Array[Vector3]
 var ordering_spots_positions: Array[Vector3]
+var free_ordering_spots: Array[Vector3]
 var eating_spots_positions: Array[Vector3]
 var exit_spot_position: Vector3
 var customer_scene=preload("res://scenes/npc/customer_npc/customer_npc.tscn")
 var customers:Array[Customer_NPC]=[]
+@onready var customer_spawn_timer:Timer=Timer.new()
+@export var customer_spawn_cooldown:float=25.0
+
 func populate_spots(waiting_container:Node3D,ordering_container:Node3D,eating_container:Node3D, exit_spot:Vector3) -> void:
 	exit_spot_position=exit_spot
 	populate_spots_array(waiting_container, waiting_spots_positions)
 	populate_spots_array(ordering_container, ordering_spots_positions)
 	populate_spots_array(eating_container, eating_spots_positions)
+	free_ordering_spots = ordering_spots_positions.duplicate() # Clona i posti inizialmente tutti liberi
 	is_initialized=true
 	
 func populate_spots_array(container: Node3D, array: Array[Vector3]) -> void:
@@ -23,21 +29,67 @@ func populate_spots_array(container: Node3D, array: Array[Vector3]) -> void:
 			array.push_back(node.global_position)
 
 func _ready() -> void:
-	pass
+	add_child(customer_spawn_timer)
+	customer_spawn_timer.wait_time=customer_spawn_cooldown
+	customer_spawn_timer.autostart=false
+	customer_spawn_timer.one_shot=true
+	customer_spawn_timer.timeout.connect(spawn_customer)
 
 #implement customer spawning logic, customer ordering and eating spots assignment logic etc...
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if not is_initialized: 
 		return
+	
+	handle_customer_spawning()
+	handle_ordering_spots_assignment()
+	
+#frees an ordering spot which had been assigned to a customer.
+#NOTE: this is meant to be called by a customer npc itself when it is done ordering
+func free_ordering_spot(spot: Vector3) -> void:
+	if not free_ordering_spots.has(spot):
+		free_ordering_spots.push_back(spot)
+
+#handles customer spawning logic
+func handle_customer_spawning()->void:
 	if customers.size()==0:
-		spawn_npc()
-	
-	if customers.size()>0:
-		customers[0].walk_to_ordering_spot(ordering_spots_positions.pick_random())
-	
-#TODO
-func spawn_npc():
+		customer_spawn_timer.start(randf_range(2.0, 7.0))
+		spawn_customer()
+	if get_no_order_customers_count()<max_no_order_customers and customer_spawn_timer.is_stopped():
+		customer_spawn_timer.start()
+
+#handles ordering spot assignment for customer npcs
+func handle_ordering_spots_assignment()->void:
+	if free_ordering_spots.size()>0:
+		for npc in customers:
+			if not npc.is_going_to_order and not npc.has_ordered and not npc.is_leaving and not npc.current_state==Customer_NPC.Customer_NPC_state.ORDER:
+				var chosen_spot=free_ordering_spots.pick_random()
+				free_ordering_spots.erase(chosen_spot)
+				npc.assigned_ordering_spot = chosen_spot
+				npc.walk_to_ordering_spot()
+				break
+
+#returns the count of customer npcs that haven't placed an order yet
+func get_no_order_customers_count()->int:
+	var count:int=customers.size()
+	for npc in customers:
+		if npc.has_ordered==true:
+			count=count-1
+			
+	return count
+
+#returns the count of customers who are ordering or are walking towards an ordering spot to order
+func get_ordering_customers_count()->int:
+	var count:int=0
+	for npc in customers:
+		if npc.is_going_to_order or npc.current_state==Customer_NPC.Customer_NPC_state.ORDER:
+			count+=1
+	return count
+
+#spawns a customer at the entrance of the level (ExitSpot)
+func spawn_customer():
 	var customer_npc:Customer_NPC=customer_scene.instantiate()
+	customers.push_back(customer_npc)
 	customer_npc.handler=self
 	add_child(customer_npc)
-	customers.push_back(customer_npc)
+	customer_npc.global_position=exit_spot_position
+	print("customer spawned")
--- 3d-food-tycoon/scenes/npc/customer_npc/customer_npc.gd
+++ 3d-food-tycoon/scenes/npc/customer_npc/customer_npc.gd
@@ -1,22 +1,25 @@
 extends CharacterBody3D
 class_name Customer_NPC
+
+#VARIABLES
 @export var movement_component: MovementComponent
 var handler:CustomerNpcManager
 @onready var navigation_agent:NavigationAgent3D=$NavigationAgent3D
-enum Customer_NPC_state{
-	WALK,
-	IDLE,
-	ORDER,
-	EAT
-}
 
+enum Customer_NPC_state{WALK,IDLE,ORDER,EAT}
 var current_state:Customer_NPC_state
+
+var assigned_ordering_spot: Vector3
 @onready var idle_timer: Timer=$IdleTimer
+@onready var ordering_timer: Timer=$OrderingTimer
+var max_ordering_time_before_crashout:float=20.0
+
 var is_going_to_order:bool=false
 var is_going_to_eat:bool=false
-
 var has_eaten:bool=false
-
+var has_ordered:bool=false
+var is_leaving:bool=false
+#METHODS
 func _ready() -> void:
 	change_state(Customer_NPC_state.WALK)
 
@@ -37,15 +40,14 @@ func change_state(new_state:Customer_NPC_state)->void:
 			#play idle animation
 		Customer_NPC_state.WALK:
 			#play walk animation
-			if not is_going_to_order and not is_going_to_eat:
+			if not is_going_to_order and not is_going_to_eat and not is_leaving:
 				walk_to_waiting_spot()
 		Customer_NPC_state.ORDER:
-			print("NIGGA IS GOING TO ORDER NOW")
-			#play order animation, show order request
-			pass
-			
+			print("customer ORDERING")
+			show_ordering_visual_effects()
+			ordering_timer.start(randf_range(ordering_timer.wait_time,ordering_timer.wait_time+6.0))
 		Customer_NPC_state.EAT:
-			#play order animation, show order request
+			#play eating animation, start timer for a few seconds, make customer leave
 			pass
 
 func _on_idle_timer_timeout() -> void:
@@ -69,22 +71,51 @@ func walk(delta:float):
 	var look_at_target:Vector3=Vector3(next_path_pos.x,global_position.y, next_path_pos.z)
 	if not global_position.is_equal_approx(look_at_target):
 		look_at(look_at_target)
-func walk_to_ordering_spot(pos: Vector3):
-	navigation_agent.target_position=pos
+func walk_to_ordering_spot():
+	navigation_agent.target_position=assigned_ordering_spot
 	is_going_to_order = true
 	change_state(Customer_NPC_state.WALK)
 
 func walk_to_eating_spot(pos: Vector3):
+	handler.free_ordering_spot(assigned_ordering_spot)
 	navigation_agent.target_position=pos
 	is_going_to_eat = true
 	change_state(Customer_NPC_state.EAT)
 
+#TODO
+func show_ordering_visual_effects()->void:
+	pass
+
 func _on_navigation_agent_3d_navigation_finished() -> void:
-	if is_going_to_order:
+	if is_going_to_order==true:
 		is_going_to_order = false
 		change_state(Customer_NPC_state.ORDER)
-	if is_going_to_eat:
+		return
+	if is_going_to_eat==true:
 		is_going_to_eat=false
 		change_state(Customer_NPC_state.EAT)
-	else:
+		return
+	if is_leaving==true:
+		remove_self_from_customers()
+		self.queue_free()
+		return
+	if current_state == Customer_NPC_state.WALK:
 		change_state(Customer_NPC_state.IDLE)
+		return
+
+#the customer gets angry and leaves because too much time elapsed
+func _on_ordering_timer_timeout() -> void:
+	print("CUSTOMER: i'm leaving, it's taking TOO MUCH TIME!")
+	#show angry animation for afew seconds, then leave
+	exit()
+	
+func exit():
+	handler.free_ordering_spot(assigned_ordering_spot)
+	navigation_agent.target_position=handler.exit_spot_position
+	is_leaving=true
+	change_state(Customer_NPC_state.WALK)
+	
+#removes self from the customers array
+func remove_self_from_customers()->void:
+	if self in handler.customers:
+		handler.customers.erase(self)
